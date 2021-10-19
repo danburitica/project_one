113f7c4f5d73e4f59ea3816e043c3a8f9835bc76
Daniel Buriticá <dani.ferbu@gmail.com>
2021-10-19 01:08:55 +0000

    Update pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/basics'
-  get 'pages/ruby'
-  get 'pages/git'
+  root 'pages#home'
+  get 'home' => 'pages#home'
+  get 'basics' => 'pages#basics'
+  get 'ruby' => 'pages#ruby'
+  get 'git' => 'pages#git'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
